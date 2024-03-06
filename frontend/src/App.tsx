e81500c95954f8d8360f3dffccdd9fefd70608ac
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-03-06 22:26:40 +1300

    add upload image boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,59 @@
-import { useState } from 'react'
+import React from 'react';
+import ImageUploading from 'react-images-uploading';
+import { useState } from 'react';
 
-import './App.css'
+export function App() {
+  const [images, setImages] = React.useState([]);
+  const maxNumber = 69;
 
-function App() {
-  
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
 
   return (
-    <>
-     <div className='container-md1'>
-      
-     </div>
-    </>
-  )
+    <div className="App">
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          // write your building UI
+          <div className="upload__image-wrapper">
+            <button
+              style={isDragging ? { color: 'red' } : undefined}
+              onClick={onImageUpload}
+              {...dragProps}
+            >
+              Click or Drop here
+            </button>
+            &nbsp;
+            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <img src={image['data_url']} alt="" width="100" />
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </ImageUploading>
+    </div>
+  );
 }
-
 export default App
