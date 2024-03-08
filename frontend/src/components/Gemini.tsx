9b24a364ae6b61f86182aaf7c525c297ffb3900c
Tajul Mazlan <tajulm@missionreadyhq.com>
2024-03-08 20:30:53 +1300

    add soemthing to gemini.tsx

---
+++ frontend/src/components/Gemini.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ImageAnnotatorClient } from '@google-cloud/vision'
+
+const Gemini = () => {
+    const client = new ImageAnnotatorClient()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Gemini
